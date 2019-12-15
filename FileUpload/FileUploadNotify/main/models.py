8ac62f57611ad429fae62c25a141ac0b717d5474
28Rahul28 <44740822+28Rahul28@users.noreply.github.com>
2019-12-15 17:45:08 +0530

    Update models.py

--- FileUpload/FileUploadNotify/main/models.py
+++ FileUpload/FileUploadNotify/main/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 import os
-from model_utils import FieldTracker
 # Create your models here.
 
 
 class FileData(models.Model):
     encrypted_data = models.CharField(max_length=264)
     UploadedFile = models.FileField(upload_to='', )
-    tracker = FieldTracker(fields=['UploadedFile'])
     def __str__(self):
         return os.path.basename(self.UploadedFile.name)
