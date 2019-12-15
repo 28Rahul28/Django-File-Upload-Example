afb83ba517b7e02db471e72dfeecdab656a55e9c
28Rahul28 <44740822+28Rahul28@users.noreply.github.com>
2019-12-15 17:44:31 +0530

    Update settings.py

--- FileUpload/FileUploadNotify/settings.py
+++ FileUpload/FileUploadNotify/settings.py
@@ -51,8 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware',
-    'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'FileUploadNotify.urls'
